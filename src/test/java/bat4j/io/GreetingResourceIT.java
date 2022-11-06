7cba9b9286331abb8d7bc193d364cd360700c00c
Bernard Rodier <bernard.rodier@gmail.com>
2022-11-06 10:57:26 +0100

    Init new quarkus projet

---
+++ src/test/java/bat4j/io/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package bat4j.io;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
